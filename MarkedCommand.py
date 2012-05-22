788dd9662c8e151883b333ad08891a3749b871a7
Paul Scott <paul.scotto@gmail.com>
2012-05-22 22:44:46 +0100

    Copy environment variables into subprocess.

--- MarkedCommand.py
+++ MarkedCommand.py
@@ -1,11 +1,13 @@
-import os
+import os, sys
 import subprocess
-
-import sublime
-import sublime_plugin
- 
+import sublime_plugin 
 
 class MarkedCommand(sublime_plugin.WindowCommand):  
     def run(self):  
         filename = self.window.active_view().file_name()
-        subprocess.call(['open', '-a', 'Marked', filename])
+
+        proc_env = os.environ.copy()
+        for k, v in proc_env.iteritems():
+            proc_env[k] = os.path.expandvars(v).encode(sys.getfilesystemencoding())
+
+        subprocess.call(['open', '-a', 'Marked', filename], env=proc_env)
